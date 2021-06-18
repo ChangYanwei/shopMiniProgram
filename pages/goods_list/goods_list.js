75b9f75b90c8217a054a9e6b6eab0c11f7719fcf
changyanwei <1926174536@qq.com>
2021-06-19 01:58:10 +0800

    自定义tab标签页组件

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -5,62 +5,36 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
+		// tab标签栏数据
+		tabs: [{
+				id: 0,
+				value: "综合"
+			},
+			{
+				id: 1,
+				value: "销量"
+			},
+			{
+				id: 2,
+				value: "价格"
+			}
+		],
+		// 当前选中的tab
+		currentIndex: 0
+	},
 
+	// 监听tab组件触发的事件
+	tabChange(event) {
+		let currentIndex = event.detail.currentIndex;
+		this.setData({
+			currentIndex
+		});
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-
+		console.log(options);
 	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
 })
