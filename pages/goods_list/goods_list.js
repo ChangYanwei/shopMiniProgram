28b5cc50a256a9ffcf5570f98b615ed1967c3c4f
changyanwei <1926174536@qq.com>
2021-06-19 23:39:09 +0800

    完成商品列表页

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,4 +1,7 @@
-// pages/goods_list/goods_list.js
+import {
+	request
+} from "../../request/index"
+
 Page({
 
 	/**
@@ -20,7 +23,19 @@ Page({
 			}
 		],
 		// 当前选中的tab
-		currentIndex: 0
+		currentIndex: 0,
+		// 请求参数
+		queryParams: {
+			query: "",
+			cid: "",
+			pagenum: 1,
+			pagesize: 10
+		},
+		// 商品总页数
+		totalPage: 0,
+		// 商品列表数据
+		goodsList: []
+
 	},
 
 	// 监听tab组件触发的事件
@@ -31,10 +46,81 @@ Page({
 		});
 	},
 
+	// 获取商品列表数据
+	getGoodsList() {
+		let {
+			queryParams,
+			goodsList
+		} = this.data;
+
+		request({
+			url: "/goods/search",
+			data: queryParams
+		}).then(res => {
+			console.log('商品列表：', res);
+			let {
+				total,
+				goods
+			} = res.message
+			// 计算出总页数
+			let totalPage = Math.ceil(total / queryParams.pagesize);
+			this.setData({
+				goodsList: goodsList.concat(goods),
+				totalPage
+			});
+
+			// 关闭下拉刷新的窗口
+			wx.stopPullDownRefresh();
+		})
+	},
+
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
 		console.log(options);
+		let queryParams = this.data.queryParams;
+		queryParams.cid = options.cat_id;
+		this.setData({
+			queryParams
+		})
+		this.getGoodsList(queryParams);
 	},
+
+	// 页面上滑，滚动条触底事件
+	onReachBottom() {
+		console.log('到底了');
+		let {
+			queryParams,
+			totalPage
+		} = this.data;
+
+		let currentPage = queryParams.pagenum;
+		if (currentPage >= totalPage) {
+			wx.showToast({
+				title: '没有更多数据了',
+				icon: "none"
+			});
+			return;
+		}
+		queryParams.pagenum += 1;
+		this.setData({
+			queryParams
+		});
+		this.getGoodsList();
+	},
+
+	// 监听下拉刷新事件
+	onPullDownRefresh() {
+		// 1.重置数组
+		// 2.重置页码
+		let queryParams = this.data.queryParams;
+		queryParams.pagenum = 1;
+		this.setData({
+			goodsList: [],
+			queryParams
+		});
+		// 3.重新发送网络请求
+		this.getGoodsList();
+	}
 })
